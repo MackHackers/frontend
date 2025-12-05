11f908a774fe99a457a6cc197e4a413ba6a21309
DarkAngel <zhitnikov0109@gmail.com>
2025-12-05 19:26:01 +0300

    fix: login styles bg

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,12 +1,20 @@
 import RegisterForm from "./components/LoginForm.tsx";
-
+import backgroundSvg from '../../icons/logo+module.svg';
 
 export default function Auth() {
     return (
-        <div
-            className="min-h-screen w-full bg-cover bg-center flex items-center justify-center p-6  bg-100"
-        >
-            <RegisterForm />
+        <div className="min-h-screen w-full flex items-center justify-center p-6 relative">
+            <div
+                className="absolute inset-0 bg-no-repeat bg-[position:20px_40px]"
+                style={{
+                    backgroundImage: `url(${backgroundSvg})`,
+                    backgroundSize: '40%',
+                    opacity: 0.9,
+                }}
+            />
+            <div className="relative z-10">
+                <RegisterForm />
+            </div>
         </div>
     );
 }
