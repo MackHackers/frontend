506cb9968925babf70eb9102aa01c742a6ba6ee6
DarkAngel <zhitnikov0109@gmail.com>
2025-12-05 17:28:40 +0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,16 @@ import LoadingPage from "./components/loading/LoadingPage.tsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import RedirectHandler from "./components/RedirectHandler/RedirectHandler.tsx";
-import AdminProtectedRoute from "./components/ProtectedRoute/AdminProtectedRoute.tsx";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.tsx";
 import DocsPage from "./pages/Docs/Docs.tsx";
 import { userService } from './api/userService';
-import DocsManagement from "./pages/DocsManagement/DocsManagment.tsx";
-import UserManagement from "./pages/UserManagment/UserManagment.tsx";
 
 // const Auth = lazy(() => import('./pages/Auth/Auth.tsx'));
 // const NotFound = lazy(() => import('./pages/NotFoundPage/NotFound.tsx'))
 
 import Auth from "./pages/Auth/Auth.tsx";
 import NotFound from "./pages/NotFoundPage/NotFound.tsx";
+import Main from "./pages/Main/Main.tsx";
+import News from "./pages/News/NewsPage.tsx";
 
 function App() {
     const [userRole, setUserRole] = useState<string>('viewer');
@@ -40,32 +38,16 @@ function App() {
                     <Routes>
                         <Route path="/" element={<RedirectHandler />} />
                         <Route path="/auth" element={<Auth />} />
+                        <Route path="/main" element={<Main />} />
+                        <Route path="/news" element={<News />} />
                         <Route
                             path="/docs"
-                            // element={<DocsPage />}
-                            element={
-                                <ProtectedRoute>
-                                    <DocsPage />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/docs-management"
-                            element={
-                                // <DocsManagement />
-                                <AdminProtectedRoute userRole={userRole}>
-                                    <DocsManagement />
-                                </AdminProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/user-management"
-                            element={
-                                // <UserManagement />
-                                <AdminProtectedRoute userRole={userRole}>
-                                    <UserManagement />
-                                </AdminProtectedRoute>
-                            }
+                            element={<DocsPage />}
+                            // element={
+                            //     <ProtectedRoute>
+                            //         <DocsPage />
+                            //     </ProtectedRoute>
+                            // }
                         />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
